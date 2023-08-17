e6d962522f770c261a258f93b4c18cc82f7ec4f6
jaszczombcodes <jastrzebska.martyna01@wp.pl>
2023-08-17 23:29:10 +0200

    navbar schema and styling

--- index.php
+++ index.php
@@ -5,11 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+
     <script src="js/jquery-3.6.4.min.js"></script>
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <nav>
+        <ul class="nav-list">
+            <li class="nav-item"><a class="button btn-blue" href="#">Track Day</a></li>
+            <ul class="nav-item nav-sub-list">
+                <li class="nav-item"><button class="button btn-dark-mode"></button></li>
+                <li class="nav-item"><a href="#" class="button">Zaloguj</a></li>
+            </ul>
+            <li class="nav-item"><a class="button btn-transparent" href="#">Racing Team</a></li>
+            <li class="nav-item"><a class="button btn-transparent" href="#">Kontakt</a></li>
+        </ul>
+    </nav>
+    <main>
+
+    </main>
+
     <script src="js/script.js"></script>
 </body>
 </html>
