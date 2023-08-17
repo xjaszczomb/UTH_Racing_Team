958f6309156bf26d4ad06898e8b05ffeca011bdd
jaszczombcodes <jastrzebska.martyna01@wp.pl>
2023-08-17 20:25:37 +0200

    init

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 
     <script src="js/jquery-3.6.4.min.js"></script>
+    <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
