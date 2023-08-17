261a91526ba9d0324c1392718c7cc263cc4fdd36
jaszczombcodes <jastrzebska.martyna01@wp.pl>
2023-08-17 20:21:39 +0200

    init

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <link rel="stylesheet" href="style.css">
+    <script src="js/jquery-3.6.4.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <script src="js/script.js"></script>
 </body>
 </html>
