25fa7266a405769a9f7f1321a9c0937200fc2ccc
jaszczombcodes <jastrzebska.martyna01@wp.pl>
2023-08-20 18:09:06 +0200

    html schema

--- index.php
+++ index.php
@@ -29,10 +29,212 @@
             <li class="nav-item"><a class="button btn-transparent" href="#">Kontakt</a></li>
         </ul>
     </nav>
-    <main>
-
+    <main class="main">
+        <img class="logo" src="img/logo.png" alt="">
+        <article class="containter">
+            <span>#01</span>
+            <h2>Track Day</h2>
+        </article>
+        <article class="containter">
+            <div class="clock">00:00</div>
+            <div class="clock-info">
+                <div class="info">
+                    <p>Czas trwania sesji</p>
+                    <p>10 min</p>
+                </div>
+                <div class="info">
+                    <p>Następna sesja</p>
+                    <p>12:35</p>
+                </div>
+            </div>
+        </article>
+        <article class="containter">
+            <header class="header">
+                <h2>Na torze</h2>
+                <img src="img/arrow.svg" alt="arrow">
+            </header>
+            <div class="table-info">
+                <p>Grupa: 1</p>
+                <p>Uczestnicy: 4</p>
+            </div>
+            <div class="table">
+                <table>
+                    <tr>
+                        <th>Imię</th>
+                        <th>Nazwisko</th>
+                        <th>Pojazd</th>
+                    </tr>
+                    <tr>
+                        <td>Abcdabcdabcd</td>
+                        <td>Abcdabcdabcd</td>
+                        <td>Abcdabcdabcd</td>
+                    </tr>
+                    <tr>
+                        <td>abcabcabca</td>
+                        <td>abcabcabca</td>
+                        <td>abcabcabca</td>
+                    </tr>
+                    <tr>
+                        <td>imie</td>
+                        <td>nazwisko</td>
+                        <td>samochod</td>
+                    </tr>
+                    <tr>
+                        <td>Martyna</td>
+                        <td>Jastrzębska</td>
+                        <td>Honda Civic</td>
+                    </tr>
+                </table>
+            </div>
+        </article>
+        <article class="containter">
+            <header class="header">
+                <h2>Oczekuje</h2>
+                <img src="img/arrow.svg" alt="arrow">
+            </header>
+            <div class="table-info">
+                <p>Grupa: 2</p>
+                <p>Uczestnicy: 4</p>
+            </div>
+            <div class="table">
+                <table>
+                    <tr>
+                        <th>Imię</th>
+                        <th>Nazwisko</th>
+                        <th>Pojazd</th>
+                    </tr>
+                    <tr>
+                        <td>Abcdabcdabcd</td>
+                        <td>Abcdabcdabcd</td>
+                        <td>Abcdabcdabcd</td>
+                    </tr>
+                    <tr>
+                        <td>abcabcabca</td>
+                        <td>abcabcabca</td>
+                        <td>abcabcabca</td>
+                    </tr>
+                    <tr>
+                        <td>imie</td>
+                        <td>nazwisko</td>
+                        <td>samochod</td>
+                    </tr>
+                    <tr>
+                        <td>Martyna</td>
+                        <td>Jastrzębska</td>
+                        <td>Honda Civic</td>
+                    </tr>
+                </table>
+            </div>
+        </article>
+        <article class="containter">
+            <header class="header">
+                <h2>Uczestnicy</h2>
+                <img src="img/arrow.svg" alt="arrow">
+            </header>
+            <div class="table-info">
+                <p>Grupy: 3</p>
+                <p>Uczestnicy: 12</p>
+            </div>
+            <div class="table">
+                <table>
+                    <tr>
+                        <th>Imię</th>
+                        <th>Nazwisko</th>
+                        <th>Pojazd</th>
+                    </tr>
+                    <tr>
+                        <td>Abcdabcdabcd</td>
+                        <td>Abcdabcdabcd</td>
+                        <td>Abcdabcdabcd</td>
+                    </tr>
+                    <tr>
+                        <td>abcabcabca</td>
+                        <td>abcabcabca</td>
+                        <td>abcabcabca</td>
+                    </tr>
+                    <tr>
+                        <td>imie</td>
+                        <td>nazwisko</td>
+                        <td>samochod</td>
+                    </tr>
+                    <tr>
+                        <td>Martyna</td>
+                        <td>Jastrzębska</td>
+                        <td>Honda Civic</td>
+                    </tr>
+                    <tr>
+                        <td>Abcdabcdabcd</td>
+                        <td>Abcdabcdabcd</td>
+                        <td>Abcdabcdabcd</td>
+                    </tr>
+                    <tr>
+                        <td>abcabcabca</td>
+                        <td>abcabcabca</td>
+                        <td>abcabcabca</td>
+                    </tr>
+                    <tr>
+                        <td>imie</td>
+                        <td>nazwisko</td>
+                        <td>samochod</td>
+                    </tr>
+                    <tr>
+                        <td>Martyna</td>
+                        <td>Jastrzębska</td>
+                        <td>Honda Civic</td>
+                    </tr>
+                    <tr>
+                        <td>Abcdabcdabcd</td>
+                        <td>Abcdabcdabcd</td>
+                        <td>Abcdabcdabcd</td>
+                    </tr>
+                    <tr>
+                        <td>abcabcabca</td>
+                        <td>abcabcabca</td>
+                        <td>abcabcabca</td>
+                    </tr>
+                </table>
+                <button>Pokaż więcej (2)</button>
+            </div>
+        </article>
+        <article class="containter">
+            <span>#02</span>
+            <h2>Racing Team</h2>
+        </article>
+        <article class="containter instagram">
+            <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/p/CbxAn9wK_Mt/?utm_source=ig_embed&amp;utm_campaign=loading" 
+            data-instgrm-version="14">
+           </blockquote> 
+        </article>
+        <article class="containter">
+            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Futhracingteam%2Fposts%2Fpfbid022WMXr8vvSuyh42Dg3o6MfFx9ZmUQQJRMihZ5ZnD4iRVZEY9RjCXNaJ8N8kRRs5s2l&show_text=true&width=500" width="500" height="761" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        </article>
+        <article class="containter">
+            <img src="img/sponsors/img1.png" alt="">
+            <img src="img/sponsors/img2.png" alt="">
+            <img src="img/sponsors/img3.png" alt="">
+            <img src="img/sponsors/img4.png" alt="">
+        </article>
+        <article class="containter">
+            <span>#03</span>
+            <h2>Kontakt</h2>
+        </article>
+        <article class="containter">
+            <form action="">
+                <input type="text" name="" id="" placeholder="Imię">
+                <input type="text" name="" id="" placeholder="Bazwisko">
+                <input type="text" name="" id="" placeholder="Email">
+                <textarea name="" id="" cols="30" rows="10" placeholder="Wiadomość"></textarea>
+            </form>
+        </article>
     </main>
+    <footer class="footer">
+        <img src="img/logo.png" alt="">
+        <div class="copyright">
+            Autor: Martyna Jastrzębska
+        </div>
+    </footer>
 
     <script src="js/script.js"></script>
+    <script async src="//www.instagram.com/embed.js"></script>
 </body>
 </html>
